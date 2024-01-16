ce22dd83ec9962376554fca727a12c3440088d87
Wanting <wantingtengtwt@gmail.com>
2024-01-15 22:27:40 -0500

    upload files api

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,25 +1,42 @@
 package com.sky.controller.admin;
 
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/admin/common")
 @Api(tags="common")
 @Slf4j
 public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
                                  //用来处理从前端传来的文件数据
     @PostMapping("/upload")
     @ApiOperation("uoload files")
     public Result<String> upload(MultipartFile file){
         log.info("uploaded file: {}",file);
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
+            //文件请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败： {}", e);
+        }
         return null;
     }
 }
