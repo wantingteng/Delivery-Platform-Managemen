2eda634a140b094fd947db0a6a83bcd6eb13b73d
Wanting <wantingtengtwt@gmail.com>
2024-01-15 23:51:25 -0500

    upload new dishes feature

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -37,6 +38,6 @@ public class CommonController {
         } catch (IOException e) {
             log.error("文件上传失败： {}", e);
         }
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,15 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+public interface DishService {
+    /**
+     * 新增菜品和口味
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+
+
+
+}
